9f15b5ff1ef72fbc36ee243c7c12c601d868bc68
Clement <clement.gutel@gmail.com>
2019-06-26 00:03:30 +1000

    Overload with HttpClient

--- GraphQLSharp.Tests/QueryTests.cs
+++ GraphQLSharp.Tests/QueryTests.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Diagnostics;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace GraphQLSharp.Tests
@@ -18,7 +19,7 @@ namespace GraphQLSharp.Tests
         [TestMethod]
         public async Task QueryAnonymous()
         {
-            var client = new GraphQLClient(new TestHttpClientFactory(), new Uri("https://swapi.apis.guru"));
+            var client = new GraphQLClient(new HttpClient(), new Uri("https://swapi.apis.guru"));
             var res = await client.QueryAsync(new GraphQLRequest(_queryAllFilms),
                                                                                     new
                                                                                     {
@@ -33,7 +34,7 @@ namespace GraphQLSharp.Tests
         [TestMethod]
         public async Task QueryDynamic()
         {
-            var client = new GraphQLClient(new TestHttpClientFactory(), new Uri("https://swapi.apis.guru"));
+            var client = new GraphQLClient(new HttpClient(), new Uri("https://swapi.apis.guru"));
             var res = await client.QueryAsync<dynamic>(new GraphQLRequest(_queryAllFilms));
             Assert.IsTrue(res.Data.allFilms.totalCount > 0);
         }
--- GraphQLSharp/Client/GraphQLClient.cs
+++ GraphQLSharp/Client/GraphQLClient.cs
@@ -13,11 +13,18 @@ namespace GraphQLSharp
     public class GraphQLClient
     {
         private readonly IHttpClientFactory _clientFactory;
+        private readonly HttpClient _client;
         private readonly Uri _uri;
         private readonly JsonSerializerSettings _serializerSettings = new JsonSerializerSettings
         {
             ContractResolver = new CamelCasePropertyNamesContractResolver()
         };
+
+        public GraphQLClient(HttpClient client, Uri uri)
+        {
+            _client = client;
+            _uri = uri;
+        }
 
         public GraphQLClient(IHttpClientFactory clientFactory, Uri uri)
         {
@@ -39,7 +46,7 @@ namespace GraphQLSharp
         {
             var queryJson = JsonConvert.SerializeObject(r, _serializerSettings);
             var httpContent = new StringContent(queryJson, Encoding.UTF8, "application/json");
-            var res = await _clientFactory.CreateClient()
+            var res = await (_client ?? _clientFactory.CreateClient())
                                           .PostAsync(_uri, httpContent, cancellationToken);
             var responseJson = await res.Content.ReadAsStringAsync();
             return JsonConvert.DeserializeObject<GraphQLResponse<T>>(responseJson, _serializerSettings);
